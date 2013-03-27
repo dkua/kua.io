0f8663332d84329782a22922ff8b0b4f660d1e6a
dkua <david.kua@mail.utoronto.ca>
2013-03-26 20:27:04 -0400

    Modified some more blog files

---
+++ pelicanconf.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"David Kua"
+SITENAME = u"A repository of my thoughts | David Kua"
+SITEURL = "http://davidkua.com"
+
+DEFAULT_LANG = u"en"
+TIMEZONE = "UTC"
+DEFAULT_DATE_FORMAT = ('%B %d, %Y')
+
+# Blogroll
+LINKS =  (("Github", "http://github.com/dkua"),
+          ("Email", "mailto:david@davidkua.com"),)
+
+DEFAULT_PAGINATION = 10
+
+THEME = "themes/dkua-bootstrap"
+
+# Allows for blogging with IPython notebooks
+PLUGINS = ["pelican.plugins.ipythonnb"]
+MARKUP = ("md", "ipynb")
