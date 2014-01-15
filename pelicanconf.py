8edb7b48895e188072eb8f3d8fb66cc5994b15b9
David Kua <david@kua.io>
2014-01-15 08:13:25 -0500

    Added CNAME file

--- pelicanconf.py
+++ pelicanconf.py
@@ -38,3 +38,6 @@ COVER_IMG_URL = "images/cover.jpg"
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
