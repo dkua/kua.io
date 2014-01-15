5b6101bda9543909d7955db358dd68358cfab562
David Kua <david@kua.io>
2014-01-15 17:07:44 -0500

    Created first post and added extra settings

---
+++ content/2014/hello_world.md
@@ -0,0 +1,28 @@
+Title: Hello world, I have cookies!
+Date: 2014-01-14
+Tags: baking, recipes
+Slug: hello-world
+
+Since I am revamping my personal site I might as well put something half useful here as my first post. For the first time I am going to be writing down my super secret cookie recipe that all my friends seem to enjoy. Also so I can just point them here when they pester me for the recipe.
+
+|   |   |
+--- | ---
+Melted Butter | 1.25 cups
+Brown Sugar | 1 cup
+White Sugar | 1 cup
+Large Egg | 2
+Vanilla Extract | 1 tsp
+Flour | 2 cup
+Oatmeal | 2 cup
+Baking Soda | 1 tsp
+Chocolate Chips | 1.75 cups
+Toffee Bits | 0.75 cup
+
+1. Mix melted butter and sugar together.
+2. Add eggs and vanilla extract to butter mixture.
+3. Combine flour, oatmeal, and baking soda in a separate bowl.
+4. Mix butter mixture with flour mixture and add the chocolate chips and toffee bits.
+5. Scoop batter onto a parchment lined cookie sheet.
+6. Bake at 350F or until golden brown.
+
+Enjoy!
--- pelicanconf.py
+++ pelicanconf.py
@@ -6,7 +6,7 @@ AUTHOR = u'David Kua'
 AUTHOR_EMAIL = 'david@davidkua.com'
 SITENAME = u'David Kua'
 SITEURL = ''
-TAGLINE = "Code, cookies, and more"
+TAGLINE = "Code and carrot cake"
 
 THEME = "../pure-single"
 
@@ -23,21 +23,34 @@ TRANSLATION_FEED_ATOM = None
 MENUITEMS = (
     ("Home", "/"),
     ("About", "/about"),
-    ("Archive", "/archive"),
+    ("Posts", "/posts"),
 )
 
+GITHUB_URL = "https://github.com/dkua"
+TWITTER_USERNAME = "davidkua"
 SOCIAL = (
-    ("github", "https://github.com/dkua"),
-    ("twitter", "https://twitter.com/davidkua"),
+    ("github", GITHUB_URL),
+    ("twitter", "http://twitter.com/" + TWITTER_USERNAME),
 )
 
 DEFAULT_PAGINATION = 10
 
-PROFILE_IMG_URL = "images/dkua_icon.png"
-COVER_IMG_URL = "images/cover.jpg"
+PROFILE_IMG_URL = "/images/dkua_icon.png"
+COVER_IMG_URL = "/images/cover.jpg"
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
 STATIC_PATHS = ['images', 'extra/CNAME']
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
+
+DATE_FORMATS = {
+    'en': '%B %d, %Y',
+}
+ARTICLE_URL = 'posts/{date:%Y}/{slug}/'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{slug}/index.html'
+ARCHIVES_SAVE_AS = "posts/index.html"
+YEAR_ARCHIVE_SAVE_AS = "posts/{date:%Y}/index.html"
+
+PAGE_URL = "{slug}/index.html"
+PAGE_SAVE_AS = "{slug}/index.html"
